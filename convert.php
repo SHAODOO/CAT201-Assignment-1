84e9ead76a6d259c9f51186d630d9f287a2f5f99
SHAODOO <ahdoo.ling010519@gmail.com>
2021-11-20 13:11:51 +0800

    create convert.php to execute java program to convert file

---
+++ convert.php
@@ -0,0 +1,9 @@
+<?php
+$command =  'javac -cp "C:\Users\User\OneDrive - Universiti Sains Malaysia\Laptop\CAT201_ASSIGNMENT_1\PDFBox\pdfbox-app-2.0.24.jar;." ReadingText.java';
+//compile java program
+shell_exec($command);
+$command2 = 'java -cp "C:\Users\User\OneDrive - Universiti Sains Malaysia\Laptop\CAT201_ASSIGNMENT_1\PDFBox\pdfbox-app-2.0.24.jar;." ReadingText';
+//run java program
+shell_exec($command2);
+include 'download.php';
+?>
