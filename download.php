f756d0ea8b57f53a35fec4756972e73abb257154
Shao Doo <ahdoo.ling010519@gmail.com>
2021-12-10 11:57:59 +0800

    Retrieve session variables that were set on previous page

--- download.php
+++ download.php
@@ -1,6 +1,17 @@
 <?php
-echo '<a href="download/text.txt" download> <button>Click here to download</button> </a>';
+// Retrieve session variables that were set on previous page
+$convertedFileName = "Converted_" . $_SESSION["uploadFileName"] . ".txt";
+
+//Display the button to let user download the converted text file
+echo '<a href="download/text.txt" download= "' . $convertedFileName . '" > <button>Click here to download</button> </a>';
+
 //Delete .pdf file
 $path = "C:\Users\User\OneDrive - Universiti Sains Malaysia\Laptop\CAT201_ASSIGNMENT_1\upload\upload.pdf";
 unlink($path);
+
+// Remove all session variables
+session_unset();
+
+// Destroy the session
+session_destroy();
 ?>
