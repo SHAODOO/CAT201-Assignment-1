0eea58e41a150e3ea3bb52b389be43499a223ed4
Shao Doo <ahdoo.ling010519@gmail.com>
2021-12-12 09:44:54 +0800

    Seperate the php tag and html tag. You can design the GUI at the body part

--- download.php
+++ download.php
@@ -2,12 +2,14 @@
 <head>
     <title>Pdf to Txt Converter</title>
 </head>
+
 <?php
 // Retrieve session variables that were set on previous page
-$convertedFileName = "Converted" . $_SESSION["uploadFileName"] . ".txt";
+$TxtFileName = "Converted" . $_SESSION["FileName"] . ".txt";
+$PdfFileName = $_SESSION["PDF"];
 
 //Display the button to let user download the converted text file
-echo '<a href="download/text.txt" download= "' . $convertedFileName . '" > <button>Click here to download</button> </a>';
+//echo '<a href="download/text.txt" download= "' . $convertedFileName . '" > <button>Click here to download</button> </a>';
 
 //Delete .pdf file
 $path = "C:\Users\User\OneDrive - Universiti Sains Malaysia\Laptop\CAT201_ASSIGNMENT_1\upload\upload.pdf";
@@ -19,4 +21,12 @@ session_unset();
 // Destroy the session
 session_destroy();
 ?>
+
+<body>
+    <!--Message to user that pdf has succesfully uploaded and converted-->
+    <p><?php echo $PdfFileName ?> has been successfully uploaded and converted. </p>
+
+    <!--Display the button to let user download the converted text file-->
+    <a href="download/text.txt" download= "<?php echo $TxtFileName ?>" > <button>Click here to download</button> </a>
+</body>
 </html>
