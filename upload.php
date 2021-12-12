b361983b4fae78624b11dd049b021a713b99b8b1
Shao Doo <ahdoo.ling010519@gmail.com>
2021-12-12 09:45:33 +0800

    Remove echo

--- upload.php
+++ upload.php
@@ -2,10 +2,12 @@
 //Start the session to record PDF file name
 session_start();
 ?>
+
 <html>
 <head>
     <title>Pdf to Txt Converter</title>
 </head>
+
 <?php
 //Specific the directory where the file is going to be placed
 $target_dir = "C:\Users\User\OneDrive - Universiti Sains Malaysia\Laptop\CAT201_ASSIGNMENT_1\upload\ ";
@@ -14,7 +16,8 @@ $target_dir = "C:\Users\User\OneDrive - Universiti Sains Malaysia\Laptop\CAT201_
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 
 //Set session variable to record PDF file name
-$_SESSION["uploadFileName"] = basename($target_file, '.pdf');
+$_SESSION["FileName"] = basename($target_file, '.pdf');
+$_SESSION["PDF"] = basename($target_file);
 
 //flag
 $successUpload = 1;
@@ -25,29 +28,25 @@ $fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 2000000) {
     echo '<script>alert("Sorry, your file is too large.")</script>';
-    //echo "Sorry, your file is too large.";
     $successUpload = 0;
 }
 
 // Allow certain file formats
 if($fileType != "pdf") {
     echo '<script>alert("Sorry, only PDF file are allowed.")</script>';
-    //echo "Sorry, only PDF file are allowed.";
     $successUpload = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($successUpload == 0) {
-    //echo "Sorry, your file was not uploaded.<br>";
     include 'index.php';
 // If everything is ok, try to upload file
 } else {
     //Rename the file to upload.pdf
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "C:\Users\User\OneDrive - Universiti Sains Malaysia\Laptop\CAT201_ASSIGNMENT_1\upload\upload.pdf")) {
-        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been successfully uploaded and converted. <br>";
         include 'convert.php';
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        echo '<script>alert("Sorry, there was an error uploading your file.")</script>';
     }
 }
 ?>
